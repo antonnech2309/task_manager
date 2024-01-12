6f527267d218d2de9c3530192ee4aec88b2a9abf
Anton <anton380969658858@gmail.com>
2024-01-12 19:49:49 +0200

    create two models Position and TasTypr both with field name

--- task_system/models.py
+++ task_system/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
