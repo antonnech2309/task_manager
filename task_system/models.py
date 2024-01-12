8e30a724dd2b18a131473a0a4fd5008205ba4cb1
Anton <anton380969658858@gmail.com>
2024-01-12 19:59:47 +0200

    create Worker model and specified __str__ method

--- task_system/models.py
+++ task_system/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -19,3 +20,12 @@ class TaskType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f"{self.username} ({self.first_name} "
+                f"{self.last_name} {self.position.name})")
+
