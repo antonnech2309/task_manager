8ce4504eb5a1c562180f8d5480fb55601c4fbb29
Anton <anton380969658858@gmail.com>
2024-01-15 20:26:53 +0200

    add function index that makes context with (num_tasks num_users num_visits) and render a template

--- task_system/views.py
+++ task_system/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_system.models import Task
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_tasks = Task.objects.count()
+    num_users = get_user_model().objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_users": num_users,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "task_system/index.html", context=context)
