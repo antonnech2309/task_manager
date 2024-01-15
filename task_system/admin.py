b821d5f9c7fadb6935de1bae8d4334f1713efaac
Anton <anton380969658858@gmail.com>
2024-01-15 13:36:10 +0200

    register Worker model and add position to list_display & define search_fields

--- task_system/admin.py
+++ task_system/admin.py
@@ -7,3 +7,27 @@ from task_system.models import Position, TaskType, Task
 
 admin.site.register(Position)
 admin.site.register(TaskType)
+
+
+@admin.register(get_user_model())
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+    search_fields = ["first_name", "last_name"]
