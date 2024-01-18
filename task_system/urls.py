daccc1568a2a420cc8a17575d63d71f55159009d
Anton <anton380969658858@gmail.com>
2024-01-18 14:48:47 +0200

    add url for worker detail page

--- task_system/urls.py
+++ task_system/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from task_system.views import index
+from task_system.views import index, WorkerDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
 ]
 
 app_name = "task_system"
