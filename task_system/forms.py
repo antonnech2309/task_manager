1aa4a385c940b5b458d2e9779b33ef5713c531bf
Anton <anton380969658858@gmail.com>
2024-01-20 11:58:05 +0200

    create form for assignees field to change it on checkbox fields and add teask_form.html template

--- task_system/forms.py
+++ task_system/forms.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+from task_system.models import Task
 
 
 class WorkerCreationForm(UserCreationForm):
@@ -10,3 +13,14 @@ class WorkerCreationForm(UserCreationForm):
             "first_name",
             "last_name",
         )
+
+
+class TaskForm(forms.ModelForm):
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
