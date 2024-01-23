8174b9a5d02b2d6f130eeaf942f322ab673d0c28
Anton <anton380969658858@gmail.com>
2024-01-23 14:08:51 +0200

    add password_change_form.html and password_change_done.html for changing password

--- task_system/views.py
+++ task_system/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
